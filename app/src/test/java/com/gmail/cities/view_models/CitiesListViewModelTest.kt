d5e3c7c7e2e7bc1f63b7f0ec89b4b057ec07717f
Ruslan <rshtukar@axon.dev>
2020-07-20 11:28:53 +0300

    Update tests.

--- app/src/test/java/com/gmail/cities/view_models/CitiesListViewModelTest.kt
+++ app/src/test/java/com/gmail/cities/view_models/CitiesListViewModelTest.kt
@@ -1,9 +1,6 @@
 package com.gmail.cities.view_models
 
-import com.gmail.cities.BaseTest
-import com.gmail.cities.TEST_CODE_ERROR
-import com.gmail.cities.TEST_FILTER_REQUEST
-import com.gmail.cities.TEST_MESSAGE_ERROR
+import com.gmail.cities.*
 import com.gmail.cities.domain.common.ResultState
 import com.gmail.cities.domain.entity.City
 import com.gmail.cities.domain.entity.ErrorState
@@ -27,8 +24,8 @@ class CitiesListViewModelTest : BaseTest() {
     @Test
     fun `get all cities`() {
         val resultSuccess: ResultState<List<City>> = ResultState.Success(listOf())
-        Mockito.`when`(useCase.getAllCities(null)).thenReturn(Observable.just(resultSuccess))
-        viewModel.getAllCities(null)
+        Mockito.`when`(useCase.getAllCities()).thenReturn(Observable.just(resultSuccess))
+        viewModel.getAllCities()
         Assert.assertEquals(resultSuccess, viewModel.getAllCitiesStatus().value)
     }
 
@@ -36,25 +33,48 @@ class CitiesListViewModelTest : BaseTest() {
     fun `get all cities error`() {
         val resultError: ResultState<List<City>> =
                 ResultState.Error(ErrorState(TEST_CODE_ERROR, TEST_MESSAGE_ERROR), emptyList())
-        Mockito.`when`(useCase.getAllCities(null)).thenReturn(Observable.just(resultError))
-        viewModel.getAllCities(null)
+        Mockito.`when`(useCase.getAllCities()).thenReturn(Observable.just(resultError))
+        viewModel.getAllCities()
         Assert.assertEquals(resultError, viewModel.getAllCitiesStatus().value)
     }
 
     @Test
-    fun `get all cities with filter`() {
-        val resultSuccess: ResultState<List<City>> = ResultState.Success(listOf())
-        Mockito.`when`(useCase.getAllCities(TEST_FILTER_REQUEST)).thenReturn(Observable.just(resultSuccess))
-        viewModel.getAllCities(TEST_FILTER_REQUEST)
-        Assert.assertEquals(resultSuccess, viewModel.getAllCitiesStatus().value)
+    fun `get filter range for A`() {
+        val filterRequest = "A"
+        val result = viewModel.getFilterResultRange(getCitiesList(), filterRequest).blockingFirst()
+        val listSize = result?.rangeTo?.minus(result.rangeFrom)?.plus(1)
+        Assert.assertEquals(4, listSize)
     }
 
     @Test
-    fun `get all cities with filter error`() {
-        val resultError: ResultState<List<City>> =
-                ResultState.Error(ErrorState(TEST_CODE_ERROR, TEST_MESSAGE_ERROR), emptyList())
-        Mockito.`when`(useCase.getAllCities(TEST_FILTER_REQUEST)).thenReturn(Observable.just(resultError))
-        viewModel.getAllCities(TEST_FILTER_REQUEST)
-        Assert.assertEquals(resultError, viewModel.getAllCitiesStatus().value)
+    fun `get filter range for Al`() {
+        val filterRequest = "Al"
+        val result = viewModel.getFilterResultRange(getCitiesList(), filterRequest).blockingFirst()
+        val listSize = result?.rangeTo?.minus(result.rangeFrom)?.plus(1)
+        Assert.assertEquals(2, listSize)
+    }
+
+    @Test
+    fun `get filter range for Alb`() {
+        val filterRequest = "Alb"
+        val result = viewModel.getFilterResultRange(getCitiesList(), filterRequest).blockingFirst()
+        val listSize = result?.rangeTo?.minus(result.rangeFrom)?.plus(1)
+        Assert.assertEquals(1, listSize)
+    }
+
+    @Test
+    fun `get filter range for ALB case insensitive`() {
+        val filterRequest = "ALB"
+        val result = viewModel.getFilterResultRange(getCitiesList(), filterRequest).blockingFirst()
+        val listSize = result?.rangeTo?.minus(result.rangeFrom)?.plus(1)
+        Assert.assertEquals(1, listSize)
+    }
+
+    @Test
+    fun `get filter range for incorrect filter`() {
+        val filterRequest = "Test"
+        val result = viewModel.getFilterResultRange(getCitiesList(), filterRequest).blockingFirst()
+        val listSize = result?.rangeTo?.minus(result.rangeFrom)
+        Assert.assertEquals(0, listSize)
     }
 }
